4630978348b61b9b03bec86481a74ef59e3e7c15
Mateusz_Ormianek <mattormianek@gmail.com>
2023-04-05 14:27:59 +0200

    Added photo and a little bit of code

--- app.py
+++ app.py
@@ -1,23 +1,29 @@
+from PIL import Image
 
-
-def letter_value(letter: str) -> int:
+def letter_value(letter: str):
     try:
         return 'abcdefghijklmnopqrstuvwxyz'.index(letter) + 1
     except ValueError:
         return "ValueError"
 
-def calculating_sum_of_word(word: str) -> int:
+def calculating_sum_of_word(word: str):
     try:
         return sum([letter_value(letter) for letter in word])
     except ValueError:
         return "ValueError"
 
+def read_png_file(filepath: str):
+    try:
+        return Image.open(filepath)
+    except FileNotFoundError:
+        return "FileNotFoundError"
 
 
 def main():
     # reading file
-    # saving to npz file
-    # loading npz file
+    photo_filepath = "esa_photo.tiff"
+    image = read_png_file(photo_filepath)
+    
     # changing values in C in that picture
     # saving to png file
 
